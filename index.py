3e132e518af939ec520ddd42bdae021a712aad5f
Andres Condo <1dkevinandres.b@gmail.com>
2020-08-24 13:40:01 -0500

    recognition of file extensions

--- index.py
+++ index.py
@@ -1,9 +1,24 @@
 # < andreescondo@gmail.com  Andres Condo>
 import datetime
 import os
+# from os.path import isfile, isdir
+import glob
+import pathlib
 
 
 def run():
+	print('''
+	********************************************
+	*    Separando los archivos en carpetas    *
+	********************************************
+	''')
+	example_dir = './'
+	directory = pathlib.Path(example_dir)
+	for fichero in directory.iterdir():
+		file = os.path.splitext(fichero.name)[1]
+		print(file)
+
+
 
 
 
