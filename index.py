fcdec6b0afc8a7230f6f0c01bd9ab206b4bee0b4
Andres Condo <1dkevinandres.b@gmail.com>
2020-08-27 16:45:53 -0500

    completed the program, creating folders and moving files to folders, separating by their corresponding extension

--- index.py
+++ index.py
@@ -1,9 +1,18 @@
 # < andreescondo@gmail.com  Andres Condo>
-from glob import glob
 from os import walk
 import pathlib
 import shutil
 import os
+
+
+def move():
+	dire, subdirs, archi = next(walk('.'))
+	for folder in subdirs:
+		for file in archi:
+			if folder == os.path.splitext(file)[1].lstrip('.'):
+				print('Moviendo... {}'.format(file))
+				shutil.move(file, folder)
+
 
 def mkdir():
 	print('''
@@ -18,42 +27,18 @@ def mkdir():
 	directory = pathlib.Path(dir_actual)
 	for fichero in directory.iterdir():
 		file.append(os.path.splitext(fichero.name)[1].lstrip('.'))
-
 		if not os.path.isdir(file[count]):
 			if not file[count] == '':
 				if not file[count] == 'py':
 					os.mkdir(file[count])
-					
 
-		# if os.path.isdir(file[count]) == os.path:
-		# 	print('hola mundo')
 		count+=1
-	print(os.path.isdir('py'))
-
+		
+	move()
 
 
 def run():
 	mkdir()
-
-	#======= VERIFICAR LOS ELEMENTOS DE LA CARPETA=======
-	dire, subdirs, archi = next(walk('.'))
-	print("Actual: ", dire)
-	print("Subdirectorios: ", subdirs)
-	print("Archivos: ", archi)
-	#====================================================
-	# algo = os.path.splitext(archi[0])[0]
-	# print(algo)
-
-	for folder in subdirs:
-		print(folder)
-		for file in archi:
-			print('--',file,'---')
-
-
-	
-
-	# shutil.move('{}.txt'.format(algo), 'txt')
-
 
 
 if __name__ == '__main__':
